32ed4ef6905388f8da09a33bac1431ed9d24ae38
Shahid-Zada <Shahidzada.cs@gmail.com>
2024-03-20 23:41:43 +0500

    3rd day

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,16 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Backend extends ChangeNotifier {
   List<List<dynamic>> habitsLists = [
     //[ habitName, hasHabitStarted, timespend(sec), timegoal(mins) ]
-    ['Exercise', false, 4, 12],
+    ['Exercise', false, 0, 12],
     ['Read', false, 2, 30],
     ['Meditate', false, 5, 32],
     ['Code', false, 10, 15],
   ];
   void hasHabitStarted(int index) {
+    var startTime = DateTime.now();
+    var elapsedTime = habitsLists[index][2];
+
     habitsLists[index][1] = !habitsLists[index][1];
     notifyListeners();
+    if (habitsLists[index][1]) {
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (!habitsLists[index][1]) {
+          timer.cancel();
+          notifyListeners();
+        }
+        var currentTime = DateTime.now();
+        habitsLists[index][2] = elapsedTime +
+            currentTime.second -
+            startTime.second +
+            60 * (currentTime.minute - startTime.minute) +
+            60 * 60 * (currentTime.hour - startTime.hour);
+        notifyListeners();
+      });
+    }
   }
 
   void settingsOpened(BuildContext context, int index) {
